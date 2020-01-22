7cc086143ea2ab2ec9d0cdff010b3f5de029ca2e
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2020-01-21 19:07:59 -0600

    adding dockerfile

--- routes/play-routes.js
+++ routes/play-routes.js
@@ -32,26 +32,9 @@ router.get('/', authCheck, (request, response) => {
             
     })
     */
-    let words = [
-        'the',   'of',     'to',    'and',   'a',      'in',    'is',
-        'it',    'you',    'that',  'he',    'was',    'for',   'on',
-        'are',   'with',   'as',    'I',     'his',    'they',  'be',
-        'at',    'one',    'have',  'this',  'from',   'or',    'had',
-        'by',    'not',    'word',  'but',   'what',   'some',  'we',
-        'can',   'out',    'other', 'were',  'all',    'there', 'when',
-        'up',    'use',    'your',  'how',   'said',   'an',    'each',
-        'she',   'which',  'do',    'their', 'time',   'if',    'will',
-        'way',   'about',  'many',  'then',  'them',   'write', 'would',
-        'like',  'so',     'these', 'her',   'long',   'make',  'thing',
-        'see',   'him',    'two',   'has',   'look',   'more',  'day',
-        'could', 'go',     'come',  'did',   'number', 'sound', 'no',
-        'most',  'people', 'my',    'over',  'know',   'water', 'than',
-        'call',  'first',  'who',   'may',   'down',   'side',  'been',
-        'now',   'find']
 
     response.render('play', {
-        user: request.user,
-        data: words
+        user: request.user
     })
 })
 
