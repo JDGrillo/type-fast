9c2b186c8903e603a5232ff95b1b0c45a8587bee
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2020-01-25 11:19:56 -0600

    cleaing up a bit

--- routes/play-routes.js
+++ routes/play-routes.js
@@ -11,28 +11,6 @@ const authCheck = (request, response, next) => {
 }
 
 router.get('/', authCheck, (request, response) => {
-    /*
-    let connectionString = "postgres://" + process.env.RDS_USER + ":" + process.env.RDS_PWD + "@"  + process.env.RDS_HOST + ":5432/" + process.env.RDS_DB
-    let pgClient = new pg.Client(connectionString)
-    async function queryIt() {
-        await pgClient.connect()
-        let words = []
-        let query = await pgClient.query("SELECT * FROM words")
-        query.rows.forEach(row => {
-            words.push(row.word)
-    })
-    return words
-    //await pgClient.end()
-    // return words
-    }
-    let queryResult =  
-            queryIt().then(function(result) {
-                console.log(result)
-                return result
-            
-    })
-    */
-
     response.render('play', {
         user: request.user
     })
